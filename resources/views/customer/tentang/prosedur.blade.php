baa9f06b92ba6307f0cb18094e7474ba0264c29d
edbertsalim <esalim279@gmail.com>
2022-06-17 07:21:33 +0700

    [Add: Tentang Kami ref #6]

--- resources/views/customer/tentang/prosedur.blade.php
+++ resources/views/customer/tentang/prosedur.blade.php
@@ -3,4 +3,59 @@
 active
 @endsection
 @section('content')
+
+<div class="heading_container heading_center">
+    <h2 class="font-weight-bold text-center" style="color:#30A139;">Prosedur</h2>
+</div>
+
+<div class="container">
+    <div class="row ">
+        <div class="col-lg-6 mb-3 mr-5">
+            <h2 class="my-3">Menjual Tanah Di Tanahin</h2>
+            <ol class="text-justify">
+                <li>Penjual mempersiapkan kelengkapan surat-surat tanah.</li>
+                <li>Jika tanah yang dijual belum lengkap, penjual akan diberikan pilihan untuk menggunakan layanan Tanahin pengurusan sertifikat
+                <li>Jika tanah yang dijual sudah lengkap, penjual akan memasukan formulir pendaftaran agar tanahnya dapat ditampilkan di marketplace
+                <li>Real Lander akan melakukan validasi ke lokasi penjualan tanah jika data yang dimasukan tidak meyakinkan
+            </ol>
+        </div>
+        <div class="col-lg-5 mb-3 ml-5">
+            <img style="width:350px;height:auto" src="{{ asset('Template/images/prosedur1.png') }}" alt="">
+        </div>
+    </div>
+
+    <div class="row ">
+        <div class="col-lg-5 mb-3 ml-5">
+            <img style="width:350px;height:auto" src="{{ asset('Template/images/prosedur2.png') }}" alt="">
+        </div>
+        <div class="col-lg-6 mb-3">
+            <br>
+            <br>
+            <h2 class="my-3">Membeli Tanah Di Tanahin</h2>
+            <ol class="text-justify">
+                <li>Pembeli memilih tanah di e-commerce Tanahin
+                <li>Pembeli dapat bertanya-tanya kepada Customer Service Tanahin mengenai tanah yang akan dibeli
+                <li>Pembeli melakukan pembayaran melalui transfer bank ke Rekening Tanahin
+                <li>Pembeli menerima kelengkapan surat-surat tanah
+                </ol>
+        </div>
+    </div>
+
+    <br>
+    <div class="row ">
+        <div class="col-lg-6 mb-3 mr-5">
+            <br>
+            <br>
+            <h2 class="my-3">Layanan Administrasi Pembuatan Sertifikat Baru atau Ganti Nama Sertifikat</h2>
+            <ol class="text-justify">
+                <li>Pengguna melakukan pengisian formulir layanan administrasi
+                <li>Admin akan menghubungi pengguna untuk menentukan jadwal survey
+                <li>Real Lander dan Mapping Expert akan datang langsung ke lokasi untuk membantu pengguna
+                <li>Pengguna dapat menunggu hasilnya            </ol>
+        </div>
+        <div class="col-lg-5 mb-3 ml-5">
+            <img style="width:350px;height:auto" src="{{ asset('Template/images/prosedur3.png') }}" alt="">
+        </div>
+    </div>
+
 @endsection
--- resources/views/customer/tentang/tentangkami.blade.php
+++ resources/views/customer/tentang/tentangkami.blade.php
@@ -4,4 +4,142 @@ active
 @endsection
 @section('content')
 
+<section class="client_section">
+
+        <div class="heading_container heading_center">
+            <h2 class="font-weight-bold text-center" style="color:#30A139;">Tentang Kami</h2>
+        </div>
+            
+        <div id="carouselExample3Controls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="box col-lg-5 mx-auto">
+                        <img style="width:300px;height:auto" src="{{ asset('Template/images/1.png') }}" alt="">
+                        <div class="detail-box">
+                            <h5 style="color:#30A139;">
+                                Siapa Kita?
+                            </h5>
+                            <p>
+                            Tanahin adalah produk yang berfokus pada lahan yang dapat membantu masyarakat Indonesia dengan basis aplikasi website.                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="carousel-item">
+                    <div class="box col-lg-5 mx-auto">
+                        <img style="width:300px;height:auto"  src="{{ asset('Template/images/2.png') }}" alt="">
+                        <div class="detail-box">
+                            <h5 style="color:#30A139;">
+                                Siapa itu Real Lander?
+                            </h5>
+                            <p>
+                            Adalah tim kami yang berada di lokasi tanah dan bertugas untuk memastikan keaslian, keamanan dan kualitas tanah.                            </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                    <div class="box col-lg-5 mx-auto">
+                        <img style="width:300px;height:auto"  src="{{ asset('Template/images/3.png') }}" alt="">
+                        <div class="detail-box">
+                            <h5 style="color:#30A139;">
+                                Siapa Itu Mo Servicer?
+                            </h5>
+                            <p>
+                                Tim kami standby 24/7 untuk menjawab segala pertanyaan seputar informasi tanah yang ingin Anda beli.
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel_btn_box">
+                    <a class="carousel-control-prev" href="#carouselExample3Controls" role="button" data-slide="prev">
+                    <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+                    <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExample3Controls" role="button" data-slide="next">
+                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                    <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                </div>
+                <br>
+    <!-- OUR TEAM -->
+        <div class="heading_container heading_center">
+            <h2 style="color:#30A139;">
+                Tanahin Team
+            </h2>
+            <!-- team baris 1 -->
+            <div class="row text-center my-5">
+                <div class="col-lg-4">
+                    <a target="_blank" href="">
+                        <img src="" width="200px" height="200px" class="rounded-circle">
+                    </a>
+                    <div class="text-center my-4">
+                        <h4 style="font-size:20px;">Kanza Azzahra</h4>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-instagram"></i></a>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-linkedin-square"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <a target="_blank" href="">
+                        <img src="" width="200px" height="200px" class="rounded-circle">
+                    </a>
+                    <div class="text-center my-4">
+                        <h4 style="font-size:20px;">Mohammad Dwicahyo Brahim</h4>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-instagram"></i></a>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-linkedin-square"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <a target="_blank" href="">
+                        <img src="" width="200px" height="200px" class="rounded-circle">
+                    </a>
+                    <div class="text-center my-4">
+                        <h4 style="font-size:20px;">Muhammad Rizal Bimantoro</h4>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-instagram"></i></a>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-linkedin-square"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <a target="_blank" href="">
+                        <img src="" width="200px" height="200px" class="rounded-circle">
+                    </a>
+                    <div class="text-center my-4">
+                        <h4 style="font-size:20px;">Fitria Dewi Wulandari</h4>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-instagram"></i></a>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-linkedin-square"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <a target="_blank" href="">
+                        <img src="" width="200px" height="200px" class="rounded-circle">
+                    </a>
+                    <div class="text-center my-4">
+                        <h4 style="font-size:20px;">Edbert Salim</h4>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-instagram"></i></a>
+                        <a class="btn" target="_blank" href=""><i class="fa fa-linkedin-square"></i></a>
+                    </div>
+                </div>
+        </div>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="{{ asset('Template/images/contact.png') }}" class="d-block w-100" alt="...">
+              </div>
+            </div>
+        </div>
+        <h2 style="color:#30A139;">
+            Dalam Pengawasan
+        </h2>
+        <img src="{{ asset('Template/images/pengawasan.png') }}" alt="...">
+        <br><br>
+
+</div>
+</section>
+      
 @endsection
